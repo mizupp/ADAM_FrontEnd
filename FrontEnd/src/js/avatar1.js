1ea9c0bcd7fdb245528201e797c05cc9a8b8644c
Mizna Uppal <miznauppal@gmail.com>
2022-12-14 04:09:01 +0000

    apple tree done

--- FrontEnd/src/js/avatar1.js
+++ FrontEnd/src/js/avatar1.js
@@ -1,15 +1,17 @@
-document.querySelector("#myFileInput").addEventListener("change", function() {
+const avatar = document.getElementById("myFileInput");
+
+avatar.addEventListener("change", function() {
     console.log(this.files);
     const reader = new FileReader();
     reader.addEventListener("load", () => {
         console.log(reader.result);
-        localStorage.setItem("recent-image", reader.result);
+        window.localStorage.setItem("recent-image", reader.result);
     });
     reader.readAsDataURL(this.files[0]);
 })   
 
 document.addEventListener("DOMContentLoaded", () => {
-    const recentImageDataUrl = localStorage.getItem("recent-image");
+    const recentImageDataUrl = window.localStorage.getItem("recent-image");
 
     if(recentImageDataUrl){
         document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl)
